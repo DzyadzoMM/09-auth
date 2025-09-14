867be5ae3c1552ef84371c28b6e32883f0423ab9
DzyadzoMM <m.dziadzo@gmail.com>
2025-09-14 20:31:57 +0300

    first

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,54 +1,32 @@
 "use client";
-
 import css from "./NoteDetails.module.css";
 import { useQuery } from "@tanstack/react-query";
+import { useParams } from 'next/navigation';
 import { fetchNoteById } from "@/lib/api";
-import { notFound } from "next/navigation";
-import { useAuthStore } from "@/lib/store/authStore";
-import { AxiosError } from 'axios';
 
-export default function NoteDetailsClient({ id }: { id: string }) {
-  const isAuth = useAuthStore((state) => state.isAuth);
+export default function NoteDetailsClient() {
+    const { id } = useParams<{ id: string }>();
 
+    
   const { data: note, isLoading, error } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
-    enabled: isAuth,
+    refetchOnMount: false,
   });
 
-  if (!isAuth) {
-    // Тут може бути логіка для неавторизованих користувачів
-    // Наприклад, редірект на сторінку входу
-  }
+  if (isLoading) return <p style={{display: "flex",  justifyContent: "center"}}>Loading, please wait...</p>;
 
-  if (isLoading) {
-    return <p style={{ display: "flex", justifyContent: "center" }}>Loading, please wait...</p>;
-  }
-  
-  if (error) {
-    // Тут перевіряємо, чи є помилка з HTTP-статусом 404
-    // Якщо так, викликаємо notFound(), щоб показати сторінку 404
-    if (error as AxiosError.response?.status === 404) {
-      notFound();
-    }
-    return <p style={{ display: "flex", justifyContent: "center" }}>Something went wrong.</p>;
-  }
+  if (error || !note) return <p style={{display: "flex",  justifyContent: "center"}}>Something went wrong.</p>;
 
-  if (!note) {
-    // Якщо нотатка не знайдена, але без помилки 404, повертаємо null
-    // щоб Next.js не показував порожній контент
-    return null;
-  }
-
-  return (
+    return (
     <div className={css.container}>
-      <div className={css.item}>
-        <div className={css.header}>
-          <h2>{note.title}</h2>
-        </div>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
-      </div>
-    </div>
-  );
+	<div className={css.item}>
+	  <div className={css.header}>
+        <h2>{note.title}</h2>
+	  </div>
+	  <p className={css.content}>{note.content}</p>
+	  <p className={css.date}>{note.createdAt}</p>
+	</div>
+</div>
+)
 }
